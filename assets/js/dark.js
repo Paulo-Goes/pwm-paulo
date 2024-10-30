11880427388c0e8ef6ea5eb4aa26baeb002c7ca5
Paulo-Goes <paulosergiolobogoes@gmail.com>
2024-10-30 08:58:34 -0300

    Rainbow mode

--- assets/js/dark.js
+++ assets/js/dark.js
@@ -1,16 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.getElementById("DMT");
+    let pressCount = localStorage.getItem('pressCount') ? parseInt(localStorage.getItem('pressCount')) : 0;
+    console.log(pressCount);
+
     if (localStorage.getItem("theme") === "dark") {
         enableDarkMode();
     }
 
     toggleButton.addEventListener("click", () => {
-        if (document.getElementById("dark-stylesheet")) {
-            disableDarkMode();
+        pressCount++;
+
+        localStorage.setItem('pressCount', pressCount);
+
+        if(pressCount >= 5){
+            enableRainbowMode();
         } else {
-            enableDarkMode();
+            if (document.getElementById("dark-stylesheet")) {
+                disableDarkMode();
+            } else {
+                enableDarkMode();
+            }
         }
+        updateButtonText();
     });
+
+    if(pressCount >= 5) {
+        enableRainbowMode();
+    }
 
     function enableDarkMode() {
         const darkModeLink = document.createElement("link");
@@ -20,12 +36,32 @@ document.addEventListener("DOMContentLoaded", () => {
         document.head.appendChild(darkModeLink);
         localStorage.setItem("theme", "dark");
     }
-
     function disableDarkMode() {
         const darkModeLink = document.getElementById("dark-stylesheet");
         if (darkModeLink) {
             darkModeLink.remove();
         }
         localStorage.setItem("theme", "light");
+        }
+    function enableRainbowMode() {
+        if (pressCount <= 5) {
+            document.body.classList.add('rainbow');
+        } else {
+            document.body.classList.remove('rainbow');
+            pressCount = 0;
+            localStorage.setItem('pressCount', pressCount);
+
+        }
+    }
+    function updateButtonText() {
+        if(pressCount === 5){
+            toggleButton.textContent = "Disable Rainbow Mode";
+        } else {
+            if (document.getElementById("dark-stylesheet")) {
+                toggleButton.textContent = "Disable Dark Mode";
+            } else {
+                toggleButton.textContent = "Enable Dark Mode";
+            }
+        }
     }
 });
